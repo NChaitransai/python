45bb30052c47c01e41cafb1cf1d1d44183aa155f
NChaitransai <nuthakkichaitrans@gmail.com>
2026-02-12 11:03:27 +0530

    Add files via upload
    python

---
+++ day1.py
@@ -0,0 +1,4 @@
+print("This is chaitran sai first day at codegnan")
+print("Today i have  learn intoduction about python")
+print(" I have lear what is python,why it is useful")
+print(" i feel very exicted to learn")
