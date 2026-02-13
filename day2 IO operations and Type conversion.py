43b67cbca8253a48e8058778b68c5ff63d2fcde0
NChaitransai <nuthakkichaitrans@gmail.com>
2026-02-13 10:56:12 +0530

    Add files via upload

---
+++ day2 IO operations and Type conversion.py
@@ -0,0 +1,25 @@
+"""
+#implict type conversion
+num1 = int(input())
+val1 = float(input())
+print(type(num1),type(val1))
+res= num1+val1
+print(res)
+"""
+"""num1 = input()
+print(type(num1))
+res= num1+10
+print(res)"""
+
+"""
+#to overcome the above problem using
+num1= input()
+print(type(num1))
+res = int(num1)+10
+print(res)
+print(type(res))"""
+ #coverting integer to float,string and boolean
+num1=10
+print(float(num1))
+print(str(num1))
+print(bool(num1))
